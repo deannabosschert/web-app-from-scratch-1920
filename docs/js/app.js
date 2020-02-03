88c7750a8ba9011ffa00c163ac2271084d8cf1f0
deannabosschert <deanna.bosschert@gmail.com>
2020-02-03 14:43:34 +0100

    fetch data

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,3 +2,7 @@ const endpoint =
   "https://api.spoonacular.com/recipes/complexSearch?query=pasta&diet=vegan&number=2";
 const apiKey = "28ff3a78659d4701a49c3345bc23c33b";
 const apiLink = endpoint + "&apiKey=" + apiKey + "&format=json";
+
+fetch(apiLink)
+  .then(res => res.json())
+  .then(res => console.log(res));
