3b38658545e4d5e4d2c4b289311864fe1eed819f
deannabosschert <deanna.bosschert@gmail.com>
2020-02-03 15:39:31 +0100

    parse json and map array

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,8 +1,29 @@
-const endpoint =
-  "https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert";
+const endpoint = "https://api.spoonacular.com/recipes/random?number=1";
 const apiKey = "28ff3a78659d4701a49c3345bc23c33b";
-const apiLink = endpoint + "&apiKey=" + apiKey + "&format=json";
+const tags = "&tags=vegetarian,dessert";
+const apiLink = endpoint + tags + "&apiKey=" + apiKey + "&format=json";
 
 fetch(apiLink)
   .then(res => res.json())
-  .then(res => console.log(res));
+  .then(json => {
+    const rawData = json.recipes;
+    // const rawData = JSON.parse('json.recipes');
+    const results = filterArray(rawData);
+    console.log(results);
+  });
+
+function filterArray(array) {
+  return array.map(results => {
+    return {
+      title: results.title,
+      instructions: results.instructions,
+      servings: results.servings,
+      dishtypes: results.dishTypes,
+      vegetarian: results.vegetarian,
+      vegan: results.vegan,
+      glutenFree: results.glutenFree,
+      dairyFree: results.dairyFree,
+      img: results.image
+    };
+  });
+}
