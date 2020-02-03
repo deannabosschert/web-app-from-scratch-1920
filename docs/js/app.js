00595ae1aea943bd251e9a95b878adbc3697c1bd
deannabosschert <deanna.bosschert@gmail.com>
2020-02-03 14:43:17 +0100

    add apikey + endpoint

---
+++ docs/js/app.js
@@ -0,0 +1,4 @@
+const endpoint =
+  "https://api.spoonacular.com/recipes/complexSearch?query=pasta&diet=vegan&number=2";
+const apiKey = "28ff3a78659d4701a49c3345bc23c33b";
+const apiLink = endpoint + "&apiKey=" + apiKey + "&format=json";
