1ba77268661806003a374ea90a631f136060a3fa
deannabosschert <deanna.bosschert@gmail.com>
2020-02-04 11:32:18 +0100

    add limit variable + inject html

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,50 +1,25 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Local_storage
-const endpoint = "https://api.spoonacular.com/recipes/random?number=1";
-const apiKey = "28ff3a78659d4701a49c3345bc23c33b";
+const endpoint = "https://api.spoonacular.com/recipes/random";
 const tags = "&tags=vegetarian,dessert";
-const apiLink = endpoint + tags + "&apiKey=" + apiKey + "&format=json";
+const apiKey = "28ff3a78659d4701a49c3345bc23c33b";
+const limit = "?number=1"
+const apiLink = endpoint + limit + tags + "&apiKey=" + apiKey + "&format=json";
 
-// fetch(apiLink)
-//   .then(res => res.json())
-//   .then(json => {
-//     const rawData = json.recipes;
-//     const data = filterArray(rawData);
-//     // pleur de data in een string en yeet 'm vervolgens in localStorage onder "recipes"
-//     localStorage.setItem("recipes", JSON.stringify(data));
-//   });
-//
-// // haal "recipes" op uit localStorage en parse ze in JSON zodat 't weer bruikbaar is (getest met wifi uitgezet)
-// const results = JSON.parse(localStorage.getItem("recipes"));
-// console.log(results);
-//
-// const d1 = document.getElementById("one");
-// d1.insertAdjacentHTML("afterend", '<div id="two">two</div>');
+fetch(apiLink)
+  .then(res => res.json())
+  .then(json => {
+    const rawData = json.recipes;
+    const data = filterArray(rawData);
+    // pleur de data in een string en yeet 'm vervolgens in localStorage onder "recipes"
+    localStorage.setItem("recipes", JSON.stringify(data));
+  });
 
-// const data = [1, 2, 3, 4];
-// const yeet = "esketit";
-// data.forEach(yeet => {
-//   element.innerHTML += "<h1>yeet</h1>";
-//   console.log(yeet);
-// });
-//
+// haal "recipes" op uit localStorage en parse ze in JSON zodat 't weer bruikbaar is (getest met wifi uitgezet)
+const results = JSON.parse(localStorage.getItem("recipes"));
+console.log(results);
 
-// // <div id="one">one</div>
-// var d1 = document.getElementsByClassName('skrt'); // or:
-// console.log(d1)
-// d1[0].insertAdjacentHTML('afterend', '<div class="two">two</div>');
-
-const name = "John";
-// assuming 'el' is an HTML DOM element
-el.innerHTML = name; // harmless in this case
-
-// ...
-
-name = "<script>alert('I am John in an annoying alert!')</script>";
-el.innerHTML = name; // harmless in this case
-
-
-// At this point, the new structure is:
-// <div id="one">one</div><div id="two">two</div>
+const d1 = document.getElementById("one");
+d1.insertAdjacentHTML("afterend", '<div id="two">two</div>');
 
 
 function filterArray(array) {
