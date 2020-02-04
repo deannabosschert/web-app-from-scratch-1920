b440b364925400fdc7a00c4237b10f666c465703
deannabosschert <deanna.bosschert@gmail.com>
2020-02-04 13:25:52 +0100

    add status code check (basic)

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,30 +1,36 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Local_storage
-const endpoint = "https://api.spoonacular.com/recipes/random";
+const endpoint = "https://api.spoonacular.com/recipes/random?number=";
 const tags = "&tags=vegetarian,dessert";
 const apiKey = "28ff3a78659d4701a49c3345bc23c33b";
 // const apiKey2 - "6ccc29bb070043dd937ef1f10dc714d3"
 // const apiKey3 = "3a0c7279c9fb4eaa96146d10ea2d6cdd"
 // const apiKey4 = "159149e54e984717be8dccce8fc3d6c9"
-const limit = "?number=1";
+const limit = "1";
 const apiLink = endpoint + limit + tags + "&apiKey=" + apiKey + "&format=json";
 
 fetch(apiLink)
-  .then(res => res.json()) // errhandling nog adden
-  .then(json => {
-    const response = json.recipes;
-    const data = filterArray(response);
+  // .then(res => res.json()) // errhandling nog adden
+  .then(function(res) {
+    if (res.status === 200) {
+      return res.json();
+    } else {
+      console.log("error 🤷");
+    }
+  })
+  .then(function(json) {
+    const results = json.recipes;
+    // console.log(res.status); // returns 200
+
+    const data = filterArray(results);
     // pleur de data in een string en yeet 'm vervolgens in localStorage onder "recipes"
-    localStorage.setItem("recipes", JSON.stringify(data));
+    return localStorage.setItem("recipes", JSON.stringify(data));
   });
 
 // haal "recipes" op uit localStorage en parse ze in JSON zodat 't weer bruikbaar is (getest met wifi uitgezet)
 const results = JSON.parse(localStorage.getItem("recipes"));
 console.log(results);
-
 const root = document.getElementById("recipe");
-// d1.insertAdjacentHTML("afterend", '<div id="two">two</div>');
 
-// if (response.status >= 200 && response.status < 400) {
 results.forEach(results => {
   root.insertAdjacentHTML(
     "afterend",
@@ -37,9 +43,6 @@ results.forEach(results => {
       '">'
   );
 });
-// } else {
-// console.log('error')
-// }
 
 function filterArray(array) {
   return array.map(results => {
