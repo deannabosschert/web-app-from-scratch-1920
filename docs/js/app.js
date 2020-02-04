f968bfe948e0dc5df058c130cd01f53ab15263c9
deannabosschert <deanna.bosschert@gmail.com>
2020-02-04 11:32:32 +0100

    add multiple apikeys

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,6 +2,9 @@
 const endpoint = "https://api.spoonacular.com/recipes/random";
 const tags = "&tags=vegetarian,dessert";
 const apiKey = "28ff3a78659d4701a49c3345bc23c33b";
+// const apiKey2 - "6ccc29bb070043dd937ef1f10dc714d3"
+// const apiKey3 = "3a0c7279c9fb4eaa96146d10ea2d6cdd"
+// const apiKey4 = "159149e54e984717be8dccce8fc3d6c9"
 const limit = "?number=1"
 const apiLink = endpoint + limit + tags + "&apiKey=" + apiKey + "&format=json";
 
