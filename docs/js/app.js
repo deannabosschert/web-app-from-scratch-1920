6ef5066b6dfac42405ab965e8f518ff41d2c39fa
deannabosschert <deanna.bosschert@gmail.com>
2020-02-10 13:52:46 +0100

    render repos

--- docs/js/app.js
+++ docs/js/app.js
@@ -15,64 +15,59 @@ const url = `https://api.github.com/users/deannabosschert/repos`;
 // const apiLink = endpoint + limit + tags + "&apiKey=" + apiKey + "&format=json";
 // const apiLink = endpoint + limit + tags + "&apiKey=" + apiKey + "&format=json";
 //
-// fetch(endpoint)
-//   // .then(res => res.json()) // correcte errhandling nog adden, zie linkje van Joost/Laurens in slack
-//   // https://codeburst.io/fetch-api-was-bringing-darkness-to-my-codebase-so-i-did-something-to-illuminate-it-7f2d8826e939
-//   .then(function(res) {
-//
-//       return res.json();
-//
-//   })
-//   .then(function(json) {
-//     // const results = json.recipes;
-//     console.log(json);
-//     // const data = filterArray(results);
-//     // pleur de data in een string en yeet 'm vervolgens in localStorage onder "recipes"
-//     // return localStorage.setItem("recipes", JSON.stringify(data));
-//   });
-
 fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data))
+  // .then(res => res.json()) // correcte errhandling nog adden, zie linkje van Joost/Laurens in slack
+  // https://codeburst.io/fetch-api-was-bringing-darkness-to-my-codebase-so-i-did-something-to-illuminate-it-7f2d8826e939
+  .then(function(res) {
 
+      return res.json();
+
+  })
+  .then(function(json) {
+    console.log(json);
+    const data = filterArray(json);
+    console.log(data)
+    // pleur de data in een string en yeet 'm vervolgens in localStorage onder "recipes"
+    return localStorage.setItem("repos", JSON.stringify(data));
+  });
 
 // haal "recipes" op uit localStorage en parse ze in JSON zodat 't weer bruikbaar is (getest met wifi uitgezet)
-// const results = JSON.parse(localStorage.getItem("recipes"));
-// console.log(results);
-// const root = document.getElementById("one");
-// root.innerHTML = "";
-//
-// results.forEach(results => {
-//   // niet beter een array.map voor gebruiken?
-//   root.insertAdjacentHTML(
-//     "afterend",
-//     "<article>" +
-//       "<h2>" +
-//       results.title +
-//       "</h2><p>" +
-//       results.instructions +
-//       '</p><img src="' +
-//       results.img +
-//       '">' +
-//       "</article>"
-//   );
-// });
-//
-// function filterArray(array) {
-//   return array.map(results => {
-//     return {
-//       title: results.title,
-//       instructions: results.instructions,
-//       servings: results.servings,
-//       dishtypes: results.dishTypes,
-//       vegetarian: results.vegetarian,
-//       vegan: results.vegan,
-//       glutenFree: results.glutenFree,
-//       dairyFree: results.dairyFree,
-//       img: results.image
-//     };
-//   });
-// }
+const results = JSON.parse(localStorage.getItem("repos"));
+console.log(results);
+const root = document.getElementById("one");
+root.innerHTML = "";
+
+results.forEach(results => {
+  // niet beter een array.map voor gebruiken?
+  root.insertAdjacentHTML(
+    "afterend",
+    "<article>" +
+      "<h2>" +
+      results.name +
+      "</h2><p>" +
+      results.description +
+      '</p><img src="' +
+      results.avatar +
+      '">' +
+      "</article>"
+  );
+});
+
+function filterArray(array) {
+  return array.map(results => {
+    return {
+      name: results.title,
+      description: results.description,
+      avatar: results.owner.avatar_url,
+      // dishtypes: results.dishTypes,
+      // vegetarian: results.vegetarian,
+      // vegan: results.vegan,
+      // glutenFree: results.glutenFree,
+      // dairyFree: results.dairyFree,
+      url: results.url
+    };
+  });
+}
 //
 // var router = {
 //   handle: function() {
