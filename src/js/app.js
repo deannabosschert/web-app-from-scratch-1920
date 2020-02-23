fb4314b2fb84c6e2c1726b209f2edeb9208105b0
deannabosschert <deanna.bosschert@gmail.com>
2020-02-23 22:32:37 +0100

    change console.log message

--- src/js/app.js
+++ src/js/app.js
@@ -9,10 +9,10 @@ import {
   (function checkLocalStorage() {
     const storage = window.localStorage
     if (storage.getItem("githubRepos") === null) {
-      console.log("geen data in je localStorage")
+      console.log("nog geen data in je localStorage, doen we zo!")
       loadNerds()
     } else {
-      console.log("wel data in je localStorage 🤓")
+      console.log("nu zit er wel data in je localStorage 🤓")
       const nerds = JSON.parse(storage.getItem("githubRepos"))
       renderNerds(nerds)
     }
