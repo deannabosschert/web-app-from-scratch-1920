7720a9dbfffd2151c24ce08148f328f594b8b1c8
deannabosschert <deanna.bosschert@gmail.com>
2020-02-24 18:49:27 +0100

    render img-avatar works

--- src/js/modules/render.js
+++ src/js/modules/render.js
@@ -7,6 +7,7 @@ export function renderNerds(nerds) {
     "%c Test!",
     "text-transform: uppercase; background: #000; color: #FFF; font-size: 21pt; font-weight: bold; padding: 5px 20px; text-shadow: -2px -2px 0 rgba(251, 1, 252, .7), 2px 2px 0 rgba(4, 251, 246, 0.7)"
   )
+
   const root = document.getElementById("overview")
   const nerdList = nerds.map(nerd => ({
     projectName: nerd.name,
@@ -18,13 +19,15 @@ export function renderNerds(nerds) {
     full_name: nerd.full_name,
     private: nerd.private,
     owner: nerd.owner,
-    html_url: nerd.html_url
+    html_url: nerd.html_url,
+    avatar: nerd.avatar
+
   }))
 
   const directives = {
     avatar: {
       src: function() {
-        return "https://s3-us-west-2.amazonaws.com/s.cdpn.io/29841/dog.jpg"
+        return this.avatar
       }
     }
   };
