0234372df8d6d0f1e53d10ef211ba39af4eebc62
deannabosschert <deanna.bosschert@gmail.com>
2020-02-28 13:08:08 +0100

    render different data

--- src/js/modules/render.js
+++ src/js/modules/render.js
@@ -7,10 +7,11 @@ const renderNerds = {
       avatar: nerd.avatar,
       html_url: nerd.html_url,
       homepage: 'Link to profile: ' + nerd.homepage,
-      id: 'Github ID: ' + nerd.id,
+      id: nerd.id,
       name: 'Name: ' + nerd.name,
       full_name: 'Full name: ' + nerd.full_name,
-      private: 'Private repo?: ' + nerd.private
+      private: 'Private repo?: ' + nerd.private,
+      linkToProfile: "Link to profile:" + nerd.id
     }))
     // const nerdAvatars = JSON.parse(storage.getItem("githubAvatars"))
     // const avatarIMG = nerdAvatars.map(nerdAvatar => ({
@@ -21,11 +22,24 @@ const renderNerds = {
       avatar: {
         src: function() {
           return this.avatar
+        },
+        href: function() {
+          return this.id
+        }
+      },
+      linkToProfile: {
+        href: function() {
+          return "#/" + this.id
         }
       },
       html_url: {
         href: function() {
           return this.html_url
+        }
+      },
+      id: {
+        href: function() {
+          return this.id
         }
       }
     }
@@ -40,7 +54,7 @@ const renderNerds = {
       projectName: 'Paginatwee' + nerd.name,
       description: 'Description: ' + nerd.description,
       homepage: 'Link to profile: ' + nerd.homepage,
-      id: 'Github ID: ' + nerd.id,
+      id: nerd.id,
       name: 'Name: ' + nerd.name,
       full_name: 'Full name: ' + nerd.full_name,
       private: 'Private repo?: ' + nerd.private
@@ -50,7 +64,7 @@ const renderNerds = {
   },
   detail: function(id) {
     console.log(id)
-    const profile = document.getElementById("profile")
+    const profile = document.getElementById("oneNerd")
     const oneNerd = id.map(nerd => ({
       projectName: nerd.name,
       description: 'Description: ' + nerd.description,
@@ -60,13 +74,37 @@ const renderNerds = {
       id: 'Github ID: ' + nerd.id,
       name: 'Name: ' + nerd.name,
       full_name: 'Full name: ' + nerd.full_name,
-      private: 'Private repo?: ' + nerd.private
+      private: 'Private repo?: ' + nerd.private,
+      milestones_url: nerd.milestones_url,
+      notifications_url: nerd.notifications_url,
+      labels_url: nerd.labels_url,
+      releases_url: nerd.releases_url,
+      deployments_url: nerd.deployments_url,
+      created_at: nerd.created_at,
+      updated_at: nerd.updated_at,
+      pushed_at: nerd.pushed_at,
+      git_url: nerd.git_url,
+      ssh_url: nerd.ssh_url,
+      clone_url: nerd.clone_url,
+      svn_url: nerd.svn_url,
+      homepage: nerd.homepage,
+      size: nerd.size,
+      stargazers_count: nerd.stargazers_count,
+      watchers_count: nerd.watchers_count,
+      language: nerd.language,
+      has_issues: nerd.has_issues,
+      has_projects: nerd.has_projects,
+      has_downloads: nerd.has_downloads,
+      has_wiki: nerd.has_wiki,
+      has_pages: nerd.has_pages,
+      forks_count: nerd.forks_count,
+      mirror_url: nerd.mirror_url,
+      archived: nerd.archived,
+      disabled: nerd.disabled,
+      open_issues_count: nerd.open_issues_count
     }))
 
-    // const avatarIMG = nerdAvatars.map(nerdAvatar => ({
-    //   avatar: nerdAvatar.transparency.model.avatar
-    // }))
-
+    console.log(oneNerd)
 
     const directives = {
       avatar: {
--- src/js/modules/store.js
+++ src/js/modules/store.js
@@ -1,6 +1,7 @@
 const store = {
   set(res) {
     const nerds = filterArray(res)
+    console.log(nerds)
     console.log("adding nerds to localStorage")
     localStorage.setItem("githubRepos", JSON.stringify(nerds))
     return nerds
@@ -26,7 +27,34 @@ function filterArray(array) {
       full_name: data.full_name,
       private: data.private,
       owner: data.owner,
-      html_url: data.html_url
+      html_url: data.html_url,
+      milestones_url: data.milestones_url,
+      notifications_url: data.notifications_url,
+      labels_url: data.labels_url,
+      releases_url: data.releases_url,
+      deployments_url: data.deployments_url,
+      created_at: data.created_at,
+      updated_at: data.updated_at,
+      pushed_at: data.pushed_at,
+      git_url: data.git_url,
+      ssh_url: data.ssh_url,
+      clone_url: data.clone_url,
+      svn_url: data.svn_url,
+      homepage: data.homepage,
+      size: data.size,
+      stargazers_count: data.stargazers_count,
+      watchers_count: data.watchers_count,
+      language: data.language,
+      has_issues: data.has_issues,
+      has_projects: data.has_projects,
+      has_downloads: data.has_downloads,
+      has_wiki: data.has_wiki,
+      has_pages: data.has_pages,
+      forks_count: data.forks_count,
+      mirror_url: data.mirror_url,
+      archived: data.archived,
+      disabled: data.disabled,
+      open_issues_count: data.open_issues_count
     }
   })
 }
